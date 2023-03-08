628da9826c33037cca4a3ed44c1c1526485f365b
Raphael <raphael.maringer@student.htldornbirn.at>
2023-03-08 09:15:54 +0100

    02-strings finished

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = 17.4
+let b = 3.9
+
+result = a / b;
+console.log("Das Ergebnis ist " + result);
+
+let vorname = "Raphael";
+let nachname = "Maringer";
+console.log("Ich heiße " + vorname + " " + nachname);
