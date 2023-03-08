002f2767e3c2537370d5cef5794116b334c7af3b
Raphael <raphael.maringer@student.htldornbirn.at>
2023-03-08 08:53:50 +0100

    changed title

---
+++ JS_Basics/01-variablen/script.js
@@ -0,0 +1,29 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+result = (a + b) / c;
+console.log("Das Ergebnis ist " + result);
+
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+
+result = a + b + c * 3;
+console.log("Das Ergebnis ist " + result);
+
+
+a = 3;
+console.log("A hat den Wert " + a);
+
+b = a + 3
+console.log("B hat den Wert " + b);
+
+c = c - 3
+console.log("C hat den Wert " + c);
