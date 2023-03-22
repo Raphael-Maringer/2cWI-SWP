3e1f9cd16b5411a9aa040847e92ad97c07463bce
Raphael <raphael.maringer@student.htldornbirn.at>
2023-03-22 09:06:05 +0100

    modified 03 Aufgabe zu Variablen

--- JS_Basics/03 Aufgabe zu Variablen/script.js
+++ JS_Basics/03 Aufgabe zu Variablen/script.js
@@ -8,6 +8,16 @@ bananaWeight = 0.22;
 pricePerApple = applePricePerKilo * appleWeight;
 pricePerBanana = bananaPricePerKilo * bananaWeight;
 
+console.log("Ein Apfel kostet " + pricePerApple + "€");
+
+console.log("Eine Banane kostet " + pricePerBanana + "€");
+
+result= 1/appleWeight;
+console.log("Ein Kilogramm Äpfel entspricht " + result + " Äpfeln");
+
+result= 1/bananaWeight;
+console.log("Ein Kilogramm Bananen entspricht " + result + " Bananen");
+
 result = pricePerApple * 8;
 console.log("8 Äpfel kosten " + result + "€");
 
